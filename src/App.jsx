0caabaa4f96f2a74cc5729d64d8fc2ec45b835c1
Mezzky <ryansahadha@gmail.com>
2024-01-01 21:02:33 +0800

    change background to dark

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
 
 const App = () => {
   return (
     <div>
-      <h1 className='text-5xl font-bold text-teal-500'>Hello World</h1>
+      <h1 className='text-5xl font-bold text-white'>Hello World</h1>
     </div>
   )
 }
