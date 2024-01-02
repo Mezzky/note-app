8a899d7b32afbbbe8662965c6d1602b5673f67ce
Rizky Ryan Sahadha <91834119+Mezzky@users.noreply.github.com>
2024-01-02 23:17:31 +0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const App = () => {
   return (
     <div className="app">
-    <h1>Catatan Belanja 📝</h1>
+    <h1>Catatan Belanj 📝</h1>
     <form className="add-form">
       <h3>Hari ini belanja apa kita?</h3>
       <div>
